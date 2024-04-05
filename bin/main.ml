3c41e3ec5ed37476fb13b4b8a0f912dad635a6e0
Vincent Liu <shuntian.liu2@cloud.com>
2024-04-05 17:08:30 +0100

    Add `votequorum` test

--- bin/main.ml
+++ bin/main.ml
@@ -14,12 +14,26 @@ let _test_cmap () =
   )
   |> ignore
 
-let () =
+let _test_quorum () =
   let open Quorum in
   ( with_handle @@ fun handle ->
     match getquorate handle with
     | Ok b ->
         Printf.printf "is quorate %b\n" b ;
+        Ok ()
+    | Error e ->
+        failwith (Cserror.to_string e)
+  )
+  |> ignore
+
+let () =
+  let open Votequorum in
+  ( with_handle @@ fun handle ->
+    match getinfo handle with
+    | Ok info ->
+        Printf.printf "total votes %d\n" (Unsigned.UInt.to_int info.total_votes) ;
+        Printf.printf "quorum %d\n" (Unsigned.UInt.to_int info.quorum) ;
+        Printf.printf "qdevice name %s\n" info.qdevice_name;
         Ok ()
     | Error e ->
         failwith (Cserror.to_string e)
